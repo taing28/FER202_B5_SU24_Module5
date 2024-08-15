8c99d313b887b03d8d54d1bc4cf24ac6477788cb
taing28 <taing280703@gmail.com>
2024-08-16 00:50:40 +0700

    update database

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Card, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function Homepage() {
@@ -29,11 +29,20 @@ export default function Homepage() {
         }
     }, [currentAlbum])
 
+    //Get all Tags
+    let tagList = [];
+    photos?.map(p => {
+        let photoTags = p.tags;
+        tagList = [...tagList, ...photoTags];
+    })
+    //Push all elements to new Array
+    let newTags = [...new Set(tagList)];
+
     useEffect(() => {
         let tempPhotos = photos;
 
         if (searchText.length > 0) {
-            tempPhotos = tempPhotos.filter(ph => ph.title.toLowerCase().startsWith(searchText.toLowerCase()))
+            tempPhotos = tempPhotos.filter(ph => ph.title.toLowerCase().includes(searchText.toLowerCase()) || ph.tags.includes(searchText.toLowerCase()))
         }
 
         if (currentTag !== "all") {
@@ -45,7 +54,7 @@ export default function Homepage() {
 
     return (
         <Row>
-            <Col md={10}>
+            <Col md={9} lg={10}>
                 <Container>
                     <Row className="menu">
                         <Col className="menu_item">
@@ -56,7 +65,7 @@ export default function Homepage() {
                         {albums?.map(album => (
                             <Col key={album.id} className="menu_item">
                                 <div onClick={() => setCurrentAlbum(album.id)}>
-                                    {album.description}
+                                    {album.title}
                                 </div>
                             </Col>
                         ))}
@@ -64,93 +73,51 @@ export default function Homepage() {
                     <Row>
                         <Col>
                             <Row>
-                                <Col>
-                                    <InputGroup className="mb-3 search-bar mt-3">
+                                <Col md={{ span: 6, offset: 3 }}>
+                                    <InputGroup className="mb-3 mt-3">
                                         <Form.Control
-                                            placeholder="Search by title"
+                                            placeholder="Search by title or tags"
                                             onChange={e => setSearchText(e.target.value)}
                                         />
                                     </InputGroup>
                                 </Col>
                             </Row>
                             <Row>
-                                {filteredPhotos?.map(photo => (
-                                    <Col md={3} key={photo.id} className="d-flex justify-content-center">
-                                        <Link to={`/photos/${photo.photoId}`} style={{ textDecoration: 'none' }}>
-                                            <Card className="photo-card">
-                                                <Card.Img variant="top" src={"assets/images/"+photo.image.thumbnail} />
-                                                <Card.Body>
-                                                    <Card.Title>{photo.title}</Card.Title>
-                                                </Card.Body>
-                                            </Card>
-                                        </Link>
-                                    </Col>
-                                ))}
+                                {
+                                    filteredPhotos.length > 0 ?
+                                        filteredPhotos?.map(photo => (
+                                            <Col md={4} xl={3} key={photo.id} className="d-flex justify-content-center">
+                                                <Link to={`/photos/${photo.photoId}`} style={{ textDecoration: 'none' }}>
+                                                    <Card className="photo-card">
+                                                        <Card.Img style={{height:"14rem"}} variant="top" src={"assets/images/" + photo.image.thumbnail} alt="Image" />
+                                                        <Card.Body>
+                                                            <Card.Title>{photo.title}</Card.Title>
+                                                        </Card.Body>
+                                                    </Card>
+                                                </Link>
+                                            </Col>
+                                        ))
+                                        :
+                                        <Col>
+                                            <h3 className="text-center">
+                                                Not found
+                                            </h3>
+                                        </Col>
+                                }
                             </Row>
                         </Col>
                     </Row>
                 </Container>
             </Col>
-            <Col md={2}>
+            <Col md={3} lg={2} className="d-none d-md-block">
                 <div className="sider rounded mt-3">
-                    <h4>Tags</h4>
-                    <ul style={{ listStyle: 'none' }}>
-                        <li>
-                            <Form.Check
-                                label="All"
-                                name="tags"
-                                type="radio"
-                                value={"all"}
-                                defaultChecked
-                                onClick={e => setCurrentTag(e.target.value)}
-                            />
-                        </li>
-                        <li>
-                            <Form.Check
-                                label="New"
-                                name="tags"
-                                type="radio"
-                                value={"new"}
-                                onClick={e => setCurrentTag(e.target.value)}
-                            />
-                        </li>
-                        <li>
-                            <Form.Check
-                                label="Hot"
-                                name="tags"
-                                type="radio"
-                                value={"hot"}
-                                onClick={e => setCurrentTag(e.target.value)}
-                            />
-                        </li>
-                        <li>
-                            <Form.Check
-                                label="Summer"
-                                name="tags"
-                                type="radio"
-                                value={"summer"}
-                                onClick={e => setCurrentTag(e.target.value)}
-                            />
-                        </li>
-                        <li>
-                            <Form.Check
-                                label="Cold"
-                                name="tags"
-                                type="radio"
-                                value={"cold"}
-                                onClick={e => setCurrentTag(e.target.value)}
-                            />
-                        </li>
-                        <li>
-                            <Form.Check
-                                label="Natural"
-                                name="tags"
-                                type="radio"
-                                value={"natural"}
-                                onClick={e => setCurrentTag(e.target.value)}
-                            />
-                        </li>
-                    </ul>
+                    <h4>Tags: {currentTag}</h4>
+                    <Button onClick={e => setCurrentTag(e.target.value)} style={{ margin: "4px" }} value={"all"}>All</Button>
+                    {
+                        newTags?.map(t => (
+                            <Button onClick={e => setCurrentTag(e.target.value)} style={{ margin: "4px" }} value={t}>{t}</Button>
+                        ))
+                    }
                 </div>
             </Col>
         </Row>
